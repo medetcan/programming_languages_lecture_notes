21ee056a7d99457674aeb248c31c350fb054eb33
medetcan <medet@canakus.com>
2019-10-19 13:52:07 +0300

    .

--- src/week-7/examples/datatypes_without_structs.rkt
+++ src/week-7/examples/datatypes_without_structs.rkt
@@ -31,3 +31,21 @@
 ; Helper functions that get the pieces for one kind of exp
 
 (define (Const-int e) (car (cdr e)))
+(define (Negate-e e) (car (cdr e)))
+(define (Add-e1 e) (car (cdr e)))
+(define (Add-e2 e) (car (cdr (cdr e))))
+(define (Multiply-e1 e) (car (cdr e)))
+(define (Multiply-e2 e) (car (cdr (cdr e))))
+
+(define (eval-exp e) (
+    cond [(Const? e) e]
+         [(Negate? e) (Const (- (Const-int (eval-exp (Negate-e e)))))]
+         [(Add? e) (let ([v1 (Const-int (eval-exp (Add-e1 e)))] [v2 (Const-int (eval-exp (Add-e2 e)))]) (Const (+ v1 v2)))]
+         [(Multiply? e) (let ([v1 (Const-int (eval-exp (Multiply-e1 e)))] [v2 (Const-int (eval-exp (Multiply-e2 e)))]) (Const (* v1 v2)))]
+         [#t (error "eval-exp expected an exp")]
+    )
+)
+
+(eval-exp (list 'Add (list 'Const 2) (list 'Const 4)))
+(eval-exp (list 'Multiply (list 'Const 2) (list 'Const 4)))
+(eval-exp (list 'Negate (list 'Multiply (list 'Const 2) (list 'Const 4))))
